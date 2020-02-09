090a903548266a55033746a4d8569fe0d28d20a1
Keerthi Siva <keerthi@swiftmedical.io>
2020-02-09 18:39:35 -0500

    add teacher with devise

--- Gemfile
+++ Gemfile
@@ -3,8 +3,6 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -15,6 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+# Use Postgres as database
 gem 'pg'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -26,6 +25,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+# User Authentication
+gem 'devise'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200208170741) do
+ActiveRecord::Schema.define(version: 20200209233215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "log_events", id: :bigserial, force: :cascade do |t|
-    t.bigint   "teacher_id"
-    t.string   "event_type",                null: false
-    t.string   "description"
-    t.datetime "logged_at",                 null: false
-    t.datetime "created_at",  precision: 6, null: false
-    t.datetime "updated_at",  precision: 6, null: false
-    t.index ["logged_at"], name: "index_log_events_on_logged_at", using: :btree
-    t.index ["teacher_id"], name: "index_log_events_on_teacher_id", using: :btree
-  end
-
-  create_table "teachers", id: :bigserial, force: :cascade do |t|
-    t.string   "email",                                default: "", null: false
-    t.string   "encrypted_password",                   default: "", null: false
+  create_table "teachers", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",             precision: 6,              null: false
-    t.datetime "updated_at",             precision: 6,              null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_teachers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "log_events", "teachers"
 end
