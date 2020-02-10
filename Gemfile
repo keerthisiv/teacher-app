13d4ddc554f63babb704cf2c8f227f7efc0c33e2
Keerthi Siva <keerthi@swiftmedical.io>
2020-02-09 22:40:35 -0500

    root to events

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 
+# Ruby
+ruby '2.6.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :log_events
   devise_for :teachers
+  root to: 'log_events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
